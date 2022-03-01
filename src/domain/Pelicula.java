c53bae255cae40bc8aecbe9e1d85c9c774071fdb
Julián Mateo Caicedo Ruiz <jul.cruizz@gmail.com>
2022-02-28 22:29:48 -0500

    Estructura de la clase Pelicula

--- src/domain/Pelicula.java
+++ src/domain/Pelicula.java
@@ -1,5 +1,30 @@
 package domain;
 
 public class Pelicula {
+	private String nombre;
+
+	// Constructores
+	public Pelicula() {
+
+	}
+
+	public Pelicula(String nombre) {
+		this.nombre = nombre;
+	}
+
+	// Getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	// Metodo toString
+	@Override
+	public String toString() {
+		return "Pelicula [Nombre: " + nombre + "]";
+	}
 
 }
